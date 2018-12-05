1d4a735ac43a4c7676541810865ecebfd7add9db
Martin Hosken <martin_hosken@sil.org>
2018-12-05 13:37:03 +0700

    Add test for Qaa? scripts to have -x- of script name

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,26 +11,38 @@ class Basic(unittest.TestCase):
             reader = csv.DictReader(csvfile)
             self.rows = list(reader)
 
-    def test_region(self):
+    def _allRows(self):
         for r in self.rows:
-            reg = r['default region']
             t = lt.LangTag(r['likely_subtag'])
             if t.lang.startswith("x-"):
                 continue
+            yield (r, t)
+
+    def test_region(self):
+        ''' Test that region values are sensible and that they equal the default region.
+            Unknown regions do not have to be specified. '''
+        for r,t in self._allRows():
+            reg = r['default region']
             if len(reg) < 2 or len(reg) > 3:
                 self.fail("{Lang_Id} has bad region: {default region}".format(**r))
-            if t.region is not None or reg != '001':
+            if t.region is not None or (reg != '001' and reg != 'XX'):
                 self.assertEqual(reg, t.region, msg="{likely_subtag} region is not {default region}".format(**r))
 
     def test_script(self):
-        for r in self.rows:
+        ''' Test script tag is appropriate and the same as the script column '''
+        for r,t in self._allRows():
             scr = r['script']
-            t = lt.LangTag(r['likely_subtag'])
-            if t.lang.startswith("x-"):
-                continue
             if len(scr) != 4:
                 self.fail("{Lang_Id} has bad script: {script}".format(**r))
             self.assertEqual(scr, t.script, msg="{likely_subtag} script is not {script}".format(**r))
+
+    def test_unknown_script(self):
+        ''' Qaa? type scripts must have an -x- for the script name '''
+        for r, t in self._allRows():
+            scr = r['script']
+            if scr.startswith("Qaa") and scr != "Qaax":
+                if t is None or 'x' not in t.extensions:
+                    self.fail("{Lang_Id} has no extension for script name".format(**r))
 
 if __name__ == "__main__":
     unittest.main()
